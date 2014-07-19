ac64ae0a7e25931bcb418daf2bee7e47b3efbf60
William Farmer <willzfarmer@gmail.com>
2014-07-19 16:00:00 -0600

    We have a page!

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome'
+    conn = sqlite3.connect('test_database.db')
+    c = conn.cursor()
+
+    data = c.execute('''SELECT * FROM records''')
+
+    x = []
+    down = []
+    up = []
+
+    for d in data:
+        x.append(d[0])
+        down.append(d[1])
+        up.append(d[2])
+
+    return render_template('index.html', domain=x, down=down, up=up)
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
